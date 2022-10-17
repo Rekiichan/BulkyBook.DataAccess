dd79dbcf1f02159e89de1fffecd0b5fd0f772d78
Rekiichan <dtnhan.ete.dut@gmail.com>
2022-10-17 19:44:19 +0700

    up 1710

--- Repository/IRepository/IRepository.cs
+++ Repository/IRepository/IRepository.cs
@@ -4,9 +4,9 @@ namespace BulkyBook.DataAccess.Repository.IRepository
 {
     public interface IRepository<T> where T : class
     {
-        T GetFirstOrDefault(Expression<Func<T,bool>> filter) ; 
+        T GetFirstOrDefault(Expression<Func<T, bool>> filter);
         IEnumerable<T> GetAll();
-         void Add(T entity);
+        void Add(T entity);
         void Remove(T entity);
         void RemoveRange(IEnumerable<T> entity);
 
